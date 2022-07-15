1c57ed9b3ace427e87e3de2164f609954b1fdb19
Vidit Shah <vidit0210@protonmail.com>
2022-07-16 03:34:58 +0530

    Basics

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div v-for="home in homes" :key="home">
+      <h1>{{ home.title }}</h1>
+      <br />
+    </div>
+  </div>
 </template>
 
 <script>
+import homes from "~/data/homes.json";
+
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    };
+  },
+};
 </script>
